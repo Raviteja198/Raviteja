ee68fc7c8c1fd2322178b50b8a3be6c2f7ee201c
Raviteja198 <93836277+Raviteja198@users.noreply.github.com>
2021-12-17 23:28:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+
+int main()
+{
+    string test;
+    test= "i am Q the omnipot3nt";
+    char ch =test[6];
+    test[17]='e';
+    cout<<test<<endl;
+    cout<<"ch= " << ch <<endl;
+
+    return 0;
+}
